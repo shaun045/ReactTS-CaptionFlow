b9697e006bb8169f6620e8e0b736ea8f2ad753fb
shaun045 <shaun1adam2@gmail.com>
2026-05-29 17:23:10 +0800

    improved subtitle layout

--- src/components/SubtitlePanel/SubtitlePanel.tsx
+++ src/components/SubtitlePanel/SubtitlePanel.tsx
@@ -8,12 +8,14 @@ export default function SubtitlePanel() {
     <div className="flex flex-col h-full w-100">
       <div className="bg-[#1b1431] flex p-3 justify-between">
         <h1 className="text-xl">Subtitle Panel</h1>
-        <button>Add</button>
+        <button className="flex p-1 px-3 rounded-md bg-[#9D2CFA]">
+          Add
+        </button>
       </div>
       <ul className="flex flex-col h-full bg-[#151027] p-3">
-        <li className="flex border p-2 w-full h-10 justify-between mb-1">
+        <li className="flex border border-gray-600 p-2 w-full h-10 justify-between mb-1 rounded-sm">
           <span>
-            minute
+            0:01:00 - 0:02:00
           </span>
           <p>Subtitle</p>
           <div className="flex gap-2">
