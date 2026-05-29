c0586954e28b915b8b5184a909dd22d37945164f
shaun045 <shaun1adam2@gmail.com>
2026-05-30 04:33:19 +0800

    improved subtitle in sub panel UI

--- src/components/SubtitlePanel/SubtitlePanel.tsx
+++ src/components/SubtitlePanel/SubtitlePanel.tsx
@@ -8,21 +8,23 @@ export default function SubtitlePanel() {
     <div className="flex flex-col h-full w-100">
       <div className="bg-[#1b1431] flex p-3 justify-between">
         <h1 className="text-xl">Subtitle Panel</h1>
-        <button className="flex p-1 px-3 rounded-md bg-[#9D2CFA]">
+        <button className="flex p-1 px-3 rounded-md bg-[#9D2CFA] hover:cursor-pointer">
           Add
         </button>
       </div>
-      <ul className="flex flex-col h-full bg-[#151027] p-3">
-        <li className="flex border border-gray-600 p-2 w-full h-10 justify-between mb-1 rounded-sm">
-          <span>
+      <ul className="flex flex-col h-full bg-[#151027] p-2">
+        <li className="flex border border-gray-600 p-2 w-full h-10 justify-between mb-1 rounded-sm items-center align-center">
+          <span className="flex text-xs text-align w-48">
             0:01:00 - 0:02:00
           </span>
-          <p>Subtitle</p>
+          <p className="flex w-full px-2">
+            Subtitle
+          </p>
           <div className="flex gap-2">
-            <button>
+            <button className="hover:cursor-pointer">
               <FaEdit/>
             </button>
-            <button>
+            <button className="hover:cursor-pointer">
               <MdDelete />
             </button>
           </div>
